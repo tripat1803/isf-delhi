a5b2f7de061872e2a99d42720fe71f941ee0f01e
tripat1803 <tripatjot1455.be21@chitkara.edu.in>
2023-12-21 09:46:54 +0530

    Updated hover anim on speaker card

--- src/components/Home/Events.js
+++ src/components/Home/Events.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SpeakerArr } from '../../utils/SpakerData';
 import { useNavigate } from 'react-router-dom';
+import { MdArrowOutward } from "react-icons/md";
 
 function SpeakerCard({ keyName, data }) {
   let navigate = useNavigate();
-  console.log(data);
+  const [style, setStyle] = useState(false);
   return (
-    <div onClick={() => {
-      navigate("/"+keyName);
+    <div onMouseEnter={() => {
+      setStyle(true);
+    }} onMouseLeave={() => {
+      setStyle(false);
+    }} onClick={() => {
+      navigate("/" + keyName);
     }} className='w-full h-[20rem] rounded-lg flex items-end p-4 relative overflow-hidden cursor-pointer'>
       <img className='absolute top-0 left-0 w-full h-full object-cover z-[1]' src={data.image} />
       <div className='bg-[linear-gradient(180deg,rgba(249,206,205,0.00)_50%,#7A3D3C_90%)] absolute top-0 left-0 w-full h-full z-[2]'></div>
-      <div className='z-[13] text-white'>
+      <div className='z-[13] text-white flex flex-col gap-2'>
         <p className='text-xl font-semibold'>{data.name}</p>
         <p className='text-sm'>{data.designation}</p>
+        <MdArrowOutward className='duration-300' style={style ? {
+          fontSize: "20px"
+        } : {
+          fontSize: "0px"
+        }} />
       </div>
     </div>
   )
